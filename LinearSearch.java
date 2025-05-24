d1c99160f226fa52472797478e33778812ec7fa3
chaitanyaph <chaitanyaphalke28@gmail.com>
2025-05-24 10:27:23 +0530

    linear search

---
+++ LinearSearch.java
@@ -0,0 +1,27 @@
+package com.arrays;
+
+public class LinearSearch {
+	
+	public static int linearSearch(int[] arr, int key) {
+		
+		for(int i = 0; i<arr.length; i++) {
+			
+			if(arr[i] == key) {
+				
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1,2,3,4,5,6};
+		
+		int linearSearch = linearSearch(arr, 4);
+		
+		System.out.println(linearSearch);
+		
+	}
+
+}
