37a39237c1d7f04d0b1d47aae0e1c55157a0654c
chaitanyaph <chaitanyaphalke28@gmail.com>
2025-05-27 00:26:19 +0530

    Convert Array to LL

---
+++ LinkedList.java
@@ -0,0 +1,53 @@
+package com.linkedlist;
+
+class Node {
+	
+	int data;
+	Node next;
+	
+	Node(int data){
+		
+		this.data = data;
+		this.next = null;
+	}
+}
+
+public class LinkedList {
+	
+	public static Node converArrToLinkedList(int[] arr) {
+		
+		Node head = new Node(arr[0]);
+		Node mover = head;
+		
+		for(int i = 1; i<arr.length; i++) {
+			
+			Node temp = new Node(arr[i]);
+			mover.next = temp;
+			mover = temp;
+		}
+		return head;
+	}
+	
+	public static void IterateLL(Node head) {
+		
+		Node temp = head;
+		
+		while(temp != null) {
+			
+			System.out.print(temp.data+" -> ");
+			temp = temp.next;
+		}
+		System.out.print("null");
+	}
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1,2,3,4,5};
+		
+		Node converArrToLinkedList = converArrToLinkedList(arr);
+		
+		IterateLL(converArrToLinkedList);
+		
+	}
+
+}
